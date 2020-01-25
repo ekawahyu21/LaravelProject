fe44fc76e5c29e49bb37d223755476b92987453b
Ekawahyu21 <45993698+Ekawahyu21@users.noreply.github.com>
2020-01-25 18:30:36 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.dashboard');
 });
 //login
 // Route::get('login', 'LoginController@index')->name('login');
